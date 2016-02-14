9f58a4c9b085ddacbf53df33f63d1dc6d481d492
Paul <paullj1@gmail.com>
2016-02-14 18:25:06 -0500

    More merge cleaning

--- common/data_store.py
+++ common/data_store.py
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-import hashlib      #used for md5 hash
-
-# class for a generic string based key-value store
-class DataItem():
-
-    # Constructor for a data item which is a value and the hash of that value
-    def __init__(self, value):
-        self.value = value
-        self.key = self.getHash(value)
-
-    # uses an MD5 hash function to return the key of the value string
-    def getHash(self, value):
-        id_string = self.value
-        return hashlib.md5(id_string.encode('utf-8')).hexdigest()
-
-'''
-    This class specifies the data storage for the node.
-'''
-class Data():
-
-    # Constructor
-    def __init__(self):
-        self.data = {}     # data storage using (key, value) pairs
-
-    # Writes a data item into this nodes hash table
-    def write(self, d_item_key, d_item_value):
-        # write the data item with updated counts
-        self.data[d_item_key] = d_item_value
-    '''
-	def read(self, d_item_key):
-		return self.data[d_item_key]
-	
-	def delete(self, d_item_key):
-		self.write(d_item_key, "")
-	'''
-    # to string function
-    def toString(self):
-        return ('\n'.join("{!s},{!r}".format(k,v) for (k,v) in self.data.items()))
-=======
 import hashlib      #used for md5 hash
 
 # class for a generic string based key-value store
@@ -81,4 +41,3 @@ class Data():
         if bool(self.data):
             return ('\n'.join("{!s},{!r}".format(k,v) for (k,v) in self.data.items()))
         return "Data is empty"
->>>>>>> 0cefe66fce0f99a1a0af9321a575243e82b4a242
--- peer/net_node.py
+++ peer/net_node.py
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import hashlib
-
-# class definition for a network node, which contains addressing at a minimum
-class NetNode:
-
-    def __init__(self, host, port):
-        self.host = host    #host address of remote machine
-        self.port = port    #port of remote machine
-        self.hash = self.getHashedID()  # a hash ID for the node
-
-    # implements MD5 hash over the string that describes the host and port of the remote node
-    def getHashedID(self):
-        id_string = self.host + str(self.port)
-        return hashlib.md5(id_string.encode('utf-8')).hexdigest().upper()
-
-    def toString(self):
-        return self.host + "," + str(self.port) + "," + self.hash
-=======
 import hashlib
 
 # class definition for a network node, which contains addressing at a minimum
@@ -34,4 +15,3 @@ class NetNode:
 
     def __str__(self):
         return "{0},{1},{2}".format(self.host,str(self.port),self.hash)
->>>>>>> 0cefe66fce0f99a1a0af9321a575243e82b4a242
